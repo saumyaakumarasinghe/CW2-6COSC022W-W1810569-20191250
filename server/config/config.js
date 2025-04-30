f3312b05549698405af8ea596588fbda83980375
Saumya Kumarasinghe <saumya.20191250@iit.ac.lk>
2025-04-30 22:02:42 +0530

    fix: env loading fixes

--- server/config/config.js
+++ server/config/config.js
@@ -1,19 +1,32 @@
-require('dotenv').config(); // Load from .env
+const dotenv = require('dotenv');
+const envFile = (() => {
+  switch (process.env.NODE_ENV) {
+    case 'production':
+      return '.env.prod';
+    case 'test':
+      return '.env.test';
+    default:
+      return '.env.dev';
+  }
+})();
+dotenv.config({ path: envFile });
+
+console.log('🔍 Sequelize CLI using DB:', process.env.DATABASE_PATH);
 
 module.exports = {
-    development: {
-        dialect: 'sqlite',
-        storage: process.env.DB_NAME || './database/dev_database.db',
-        logging: console.log,
-    },
-    test: {
-        dialect: 'sqlite',
-        storage: process.env.DB_NAME || './database/test_database.db',
-        logging: false,
-    },
-    production: {
-        dialect: 'sqlite',
-        storage: process.env.DB_NAME || './database/prod_database.db',
-        logging: false,
-    },
+  development: {
+    dialect: 'sqlite',
+    storage: process.env.DATABASE_PATH,
+    logging: console.log,
+  },
+  test: {
+    dialect: 'sqlite',
+    storage: process.env.DATABASE_PATH,
+    logging: false,
+  },
+  production: {
+    dialect: 'sqlite',
+    storage: process.env.DATABASE_PATH,
+    logging: false,
+  },
 };
