da66ca236a002a01ee6dbdce2d0ec3a4025b2684
Saumya Kumarasinghe <saumya.20191250@iit.ac.lk>
2025-04-30 22:02:13 +0530

    chore: swagger setup

--- server/app.js
+++ server/app.js
@@ -1,14 +1,14 @@
 // Dynamically load the correct .env file based on NODE_ENV
 const dotenv = require('dotenv');
 const envFile = (() => {
-    switch (process.env.NODE_ENV) {
-        case 'production':
-            return '.env.prod';
-        case 'test':
-            return '.env.test';
-        default:
-            return '.env.dev';
-    }
+  switch (process.env.NODE_ENV) {
+    case 'production':
+      return '.env.prod';
+    case 'test':
+      return '.env.test';
+    default:
+      return '.env.dev';
+  }
 })();
 dotenv.config({ path: envFile }); // Load the correct .env file
 console.log(`🌱 Loading environment variables from: ${envFile}`);
@@ -27,13 +27,13 @@ const PORT = process.env.PORT || 8000; // Default to port 8000 if PORT is not se
 
 // Enable CORS for development
 app.use(
-    cors({
-        origin:
-            process.env.NODE_ENV === 'production'
-                ? 'your-production-domain'
-                : ['http://localhost:3000', 'http://localhost:3001'],
-        credentials: true,
-    })
+  cors({
+    origin:
+      process.env.NODE_ENV === 'production'
+        ? 'your-production-domain'
+        : ['http://localhost:3000', 'http://localhost:3001'],
+    credentials: true,
+  })
 );
 
 // Middleware to parse JSON and form data
@@ -42,7 +42,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Swagger setup
 if (process.env.NODE_ENV !== 'production') {
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 }
 
 // Configure routes
@@ -51,8 +51,8 @@ app.use('/api/v1', routesV1);
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`🚀 Server is running on http://localhost:${PORT}`);
-    if (process.env.NODE_ENV !== 'production') {
-        console.log(`🧩 Swagger docs available at http://localhost:${PORT}/api-docs`);
-    }
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+  if (process.env.NODE_ENV !== 'production') {
+    console.log(`🧩 Swagger docs available at http://localhost:${PORT}/api-docs`);
+  }
 });
