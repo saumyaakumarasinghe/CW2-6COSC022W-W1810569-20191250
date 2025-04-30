0e879615fdded6ad78525c27952153c3fbc29626
Saumya Kumarasinghe <saumya.20191250@iit.ac.lk>
2025-04-30 22:01:10 +0530

    feat: common endpoints to check health of the server

--- server/routes/common.routes.js
+++ server/routes/common.routes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const commonRoutes = express.Router();
 
 commonRoutes.get('/health', (req, res) => {
-    res.status(200).json({
-        status: 'OK',
-        timestamp: new Date().toISOString(),
-    });
+  res.status(200).json({
+    status: 'OK',
+    timestamp: new Date().toISOString(),
+  });
 });
 
 module.exports = commonRoutes;
