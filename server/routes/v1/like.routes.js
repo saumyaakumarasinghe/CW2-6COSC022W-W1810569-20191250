7f7c2be7f9a12e2f190842bc4e5ac8c6b8a7643f
saumya-kumarasinghe <saumya.20191250@iit.ac.lk>
2025-05-20 22:35:04 +0530

    fix: path changes

--- server/routes/v1/like.routes.js
+++ server/routes/v1/like.routes.js
@@ -5,9 +5,9 @@ const likeController = require('../../controllers/like.controller');
 const { checkAuth } = require('../../middleware/check-token.middleware');
 
 // Toggle like/unlike for a post
-router.post('/:postId/like', checkAuth, likeController.toggleLike);
+router.post('/:postId', checkAuth, likeController.toggleLike);
 
 // Get likes for a specific post
-router.get('/:postId/likes', likeController.getPostLikes);
+router.get('/:postId', likeController.getPostLikes);
 
 module.exports = router;
